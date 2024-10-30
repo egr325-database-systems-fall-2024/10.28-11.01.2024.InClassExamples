40869a49e79ec91b2706fc085af9027203a49c37
Larry Clement <lclement@calbaptist.edu>
2024-10-30 15:24:24 -0700

    After class on 10/30/2024

--- Key/Stored Procedures/02.spDisplayMessage.sql
+++ Key/Stored Procedures/02.spDisplayMessage.sql
@@ -1,6 +1,9 @@
+use ap;
 DROP PROCEDURE IF EXISTS test;
 
--- Change statement delimiter from semicolon to double front slash (needed because the semicolon is used within the CREATE PROCEDURE statement, it allows // to define the end of the CREATE PROCEDURE statement (it is also common to use $$)
+-- Change statement delimiter from semicolon to double front slash 
+-- (needed because the semicolon is used within the CREATE PROCEDURE statement, 
+-- it allows // to define the end of the CREATE PROCEDURE statement (it is also common to use $$)
 DELIMITER //
 
 CREATE PROCEDURE test()
