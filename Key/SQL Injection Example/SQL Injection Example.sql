1916f5e045e1bc5fb6ebb680072dd4eedc8a8cd7
Larry Clement <lclement@calbaptist.edu>
2024-11-01 15:58:22 -0700

    After class on 11/1/2024

--- Key/SQL Injection Example/SQL Injection Example.sql
+++ Key/SQL Injection Example/SQL Injection Example.sql
@@ -1,10 +1,11 @@
 use ap;
 
 SET @username = 'haki';
-SET @password = 'wrong password';-- 
--- SET @password = 'any string'' OR ''x''=''x';
+-- SET @password = 'wrong password';-- 
+SET @password = 'any string'' OR ''x''=''x';
 
-SET @sql = CONCAT('SELECT * FROM users WHERE username=''', @username, ''' AND password=''', @password, ''''); 
+SET @sql = CONCAT('SELECT * FROM users WHERE username=''', 
+	@username, ''' AND password=''', @password, ''''); 
 SELECT @sql;
      
 PREPARE dynamic_statement FROM @sql;
